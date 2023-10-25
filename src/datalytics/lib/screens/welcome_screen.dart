b8948789865ea2064d6b26cadfdfddca4db58944
Mozart Rueckert <mozartrue@outlook.com>
2023-10-25 20:22:57 -0300

    feat: criado botoes de autenticacao

--- src/datalytics/lib/screens/welcome_screen.dart
+++ src/datalytics/lib/screens/welcome_screen.dart
@@ -1,3 +1,5 @@
+import 'package:datalytics/components/labeled_button.dart';
+import 'package:datalytics/components/main_button.dart';
 import 'package:datalytics/constants.dart';
 import 'package:flutter/material.dart';
 
@@ -51,6 +53,42 @@ class WelcomeScreen extends StatelessWidget {
                           decoration: BoxDecoration(
                             color: dtlBlack.withOpacity(0.5),
                             borderRadius: const BorderRadius.all(Radius.circular(20)),
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Image.asset('assets/images/complete-logo.png',),
+                              Column(
+                                children: [
+                                  LabeledButton(
+                                    labelText: 'Já possui uma conta?',
+                                    button: MainButton(
+                                      title: 'Login',
+                                      textColor: dtlGrey100,
+                                      buttonColor: dtlGreyBlue,
+                                      width: (size.width / 4) - 60,
+                                      height: 40,
+                                      borderRadius: 8,
+                                      onPress: (){},
+                                    ), 
+                                  ),
+                                  const SizedBox(height: 20,),
+                                  LabeledButton(
+                                    labelText: 'Ainda não possui uma conta?',
+                                    button: MainButton(
+                                      title: 'Cadastra-se',
+                                      textColor: dtlGrey100,
+                                      buttonColor: dtlGreyBlue,
+                                      width: (size.width / 4) - 60,
+                                      height: 40,
+                                      borderRadius: 8,
+                                      onPress: (){},
+                                    ), 
+                                  ),
+                                  SizedBox(height: size.height * 0.06,)
+                                ],
+                              ),
+                            ],
                           ),
                         ),
                       ],
