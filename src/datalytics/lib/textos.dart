74af2a138e483654da81763c5ff8ca47d0732f2e
Mozart Rueckert <mozartrue@outlook.com>
2023-10-25 21:10:39 -0300

    feat: definido contante texto para display na welcome

---
+++ src/datalytics/lib/textos.dart
@@ -0,0 +1,5 @@
+const String welcomeText = '''
+Oferecemos recursos, conhecimento e acesso aos melhores consultores para otimizar seus processos de coleta e análise de dados. Conectamos você aos especialistas em gestão de dados, permitindo que sua empresa alcance o sucesso por meio de insights precisos e estratégias eficazes."
+
+Nossa plataforma é projetada para atender às necessidades de empresas de todos os tamanhos e setores. Seja você uma pequena startup buscando otimizar seus processos de coleta e análise de dados ou uma grande corporação interessada em implementar estratégias de governança de dados, temos os recursos e o conhecimento necessários para ajudá-lo. Além disso, orgulhamo-nos de contar com os melhores consultores em gestão de dados, prontos para fornecer orientação especializada e personalizada para sua empresa. Estabelecemos a ponte entre consultores de renome e sua organização, garantindo que você tenha acesso às mentes mais brilhantes no mundo da gestão de dados.
+''';
