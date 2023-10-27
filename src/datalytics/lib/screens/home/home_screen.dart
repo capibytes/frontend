46923c9642dc45512d2ddcb8c5b0cd5ddfa428da
Mozart Rueckert <mozartrue@outlook.com>
2023-10-27 18:50:51 -0300

    feat: refatorado app header

--- src/datalytics/lib/screens/home/home_screen.dart
+++ src/datalytics/lib/screens/home/home_screen.dart
@@ -1,4 +1,4 @@
-import 'package:datalytics/constants.dart';
+import 'package:datalytics/components/header.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -6,79 +6,16 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return Scaffold(
-      body: Column(
+    return const Scaffold(
+      body: Stack(
         children: [
-          Container(
-            width: size.width,
-            height: 68,
-            color: dtlBrown300,
-            padding: const EdgeInsets.symmetric(horizontal: 48),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  'Datalytics',
-                  style: TextStyle(
-                    fontSize: 26,
-                    color: dtlGreyBlue,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    const TopBarButtonTextOption(title: 'Blog'),
-                    const TopBarButtonTextOption(title: 'LGPD'),
-                    const TopBarButtonTextOption(title: 'Sobre'),
-                    const TopBarButtonTextOption(title: 'Consultores'),
-                    Container(
-                      width: 48,
-                      height: 48,
-                      decoration: BoxDecoration(
-                        color: dtlGrey100,
-                        borderRadius: BorderRadius.circular(200),
-                      ),
-                      child: const Icon(
-                        Icons.person,
-                        size: 32,
-                        color: dtlBlack,
-                      ),
-                    ),
-                  ],
-                )
-              ],
-            ),
+          Column(
+            children: [
+              Header(),
+            ],
           ),
         ],
-      ),
-    );
-  }
-}
-
-
-class TopBarButtonTextOption extends StatelessWidget {
-  final String title;
-  final VoidCallback? onPress;
-  const TopBarButtonTextOption({
-    required this.title,
-    this.onPress,
-    super.key
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return TextButton(
-      onPressed: onPress,
-      child: Text(
-        title,
-        style: const TextStyle(
-          color: dtlGreyBlue,
-          fontSize: 16,
-          fontWeight: FontWeight.w200,
-        ),
-      ),
+      )
     );
   }
 }
