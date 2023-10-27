cc3acc9f846f11041d0d212afb96428442b7464b
Mozart Rueckert <mozartrue@outlook.com>
2023-10-27 19:57:27 -0300

    feat: add texto statico encontar consultores

--- src/datalytics/lib/screens/home/home_screen.dart
+++ src/datalytics/lib/screens/home/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:datalytics/components/gradient_background.dart';
 import 'package:datalytics/components/header.dart';
+import 'package:datalytics/components/main_button.dart';
+import 'package:datalytics/constants.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -7,13 +9,52 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
       body: Stack(
         children: [
-          GradientBackground(),
+          const GradientBackground(),
           Column(
             children: [
-              Header(),
+              const Header(),
+              const SizedBox(height: 40,),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 60),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(
+                              width: size.width / 4,
+                              child: const Text(
+                                '''Encontre um consultor adequado para ajudar a sua empresa na gestão de dados.''',
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: 3,
+                                style: TextStyle(
+                                  color: dtlWhite,
+                                  fontSize: 18,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(height: 12,),
+                            const MainButton(
+                              title: 'Encontrar',
+                              buttonColor: dtlGreyBlue,
+                              textColor: dtlWhite,
+                              borderRadius: 8,
+                              width: 120,
+                              height: 40,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ],
