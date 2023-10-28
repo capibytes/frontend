592470aee2b6852a730fe41b7be4651bfbfaa6be
Mozart Rueckert <mozartrue@outlook.com>
2023-10-27 22:43:01 -0300

    feat: create consultor tile para display de consultor

---
+++ src/datalytics/lib/components/consultor_tile.dart
@@ -0,0 +1,74 @@
+import 'package:datalytics/constants.dart';
+import 'package:datalytics/models/consultor_model.dart';
+import 'package:flutter/material.dart';
+
+class ConsultorTile extends StatelessWidget {
+  final ConsultorModel consultor;
+  final VoidCallback? onPress;
+  const ConsultorTile({
+    required this.consultor,
+    this.onPress,
+    super.key
+  });
+
+  TextStyle getTextStyle({bool bold = false, double fontSize = 14}) {
+    return TextStyle(
+      fontSize: fontSize,
+      fontWeight: bold ? FontWeight.w400 : FontWeight.normal,
+      color: dtlWhite,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 140,
+      height: 70,
+      child: ListTile(
+        onTap: onPress,
+        title: Container(
+          decoration: BoxDecoration(
+            gradient: const LinearGradient(
+              colors: [
+                dtlGrey100,
+                dtlBlue800,
+              ],
+              begin: FractionalOffset(0, 0),
+              end: FractionalOffset(0, 1),
+              stops: [0, 1],
+              tileMode: TileMode.clamp,
+            ),
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const SizedBox(width: 8,),
+              Image.asset(
+                consultor.avatarUrl,
+              ),
+              const SizedBox(width: 14,),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text(
+                    consultor.name,
+                    style: getTextStyle(bold: true, fontSize: 16),
+                  ),
+                  Text(
+                    consultor.type,
+                    style: getTextStyle(),
+                  ),
+                  Text(
+                    consultor.job,
+                    style: getTextStyle(),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
