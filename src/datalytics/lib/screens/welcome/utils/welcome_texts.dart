abc0fa4cf00e271ce6890bc09067e4be05528a89
mozart-rueckert <suporte4@grupoinovasse.com.br>
2023-10-29 23:31:59 -0300

    feat: definido textos utilizados na welcome page

---
+++ src/datalytics/lib/screens/welcome/utils/welcome_texts.dart
@@ -0,0 +1,42 @@
+class WelcomeTexts {
+  static String firstText = '''
+Forjando Estratégias 
+Através da Gestão de Dados.
+
+Transforme seus dados em ativos estratégicos. Nossa plataforma é o seu guia completo para a gestão de dados empresariais.
+  ''';
+
+  static String whatIsFirstBloc = '''
+  Oferecemos recursos, conhecimento e acesso aos melhores consultores para otimizar seus processos de coleta e análise de dados. Conectamos você aos especialistas em gestão de dados, permitindo que sua empresa alcance o sucesso por meio de insights precisos e estratégias eficazes."
+
+  Nossa plataforma é projetada para atender às necessidades de empresas de todos os tamanhos e setores. Seja você uma pequena startup buscando otimizar seus processos 
+  de coleta e análise de dados ou uma grande corporação interessada em implementar estratégias de governança de dados, temos os recursos e o conhecimento necessários 
+  para ajudá-lo. 
+
+  Além disso, orgulhamo-nos de contar com os melhores consultores em gestão de dados, prontos para fornecer orientação especializada e personalizada para sua empresa. Estabelecemos a ponte entre consultores de renome e sua organização, garantindo 
+  que você tenha acesso às mentes mais brilhantes no mundo da gestão de dados.
+  ''';
+
+
+  static String whatIsSecondBloc = '''
+    Em um mundo movido pela constante evolução tecnológica e pela crescente importância dos dados, a governança de dados emerge como uma trama complexa que molda o destino das organizações. Neste cenário enigmático, as empresas se veem diante do desafio de equilibrar as necessidades de coleta, armazenamento e uso de informações sensíveis.
+
+    A jornada pela governança de dados é como um nevoeiro que encobre a paisagem, onde a visibilidade é limitada e os caminhos são obscuros. À medida que as organizações embarcam nessa narrativa, descobrem que é preciso navegar por um labirinto de regulamentações, políticas internas e estruturas de controle. Às vezes, os protagonistas dessa história se sentem como personagens perdidos em uma trama complexa, buscando entender as complexidades da conformidade e da segurança de dados.
+
+
+    ''';
+
+
+  static String whatIsThirdBloc = 'No entanto, há uma luz tênue que ilumina esse cenário enigmático. A governança de dados, apesar de suas complexidades, oferece a promessa de um tesouro escondido: a capacidade de transformar informações caóticas em ativos valiosos. À medida que a história se desenrola, as organizações começam a perceber que, por meio da governança eficaz, podem ganhar a confiança de seus clientes, melhorar a tomada de decisões e alcançar uma posição mais vantajosa em um mercado altamente competitivo.';
+
+  static String whatIsFourthBloc = '''
+  Acreditamos que os dados são o ativo mais valioso de qualquer organização, e é por isso que estamos empenhados em ajudar você a extrair o máximo valor de seus dados. Nossos consultores experientes estão disponíveis para orientá-lo em todas as etapas do processo, desde a definição de estratégias de coleta de dados até a implementação de práticas sólidas de governança de dados.
+
+  A decisão de adotar nossa plataforma e aproveitar nossos serviços de consultoria em gestão de dados representa um investimento estratégico no crescimento e no sucesso contínuo de sua empresa. Convidamos você a permitir que nossa equipe o guie na transformação de dados em insights valiosos e na implementação de estratégias eficazes. 
+
+  Não hesite em entrar em contato conosco ainda hoje para dar início à sua jornada em direção a um futuro mais orientado por dados e, consequentemente, mais bem-sucedido. Estamos à disposição para ajudar a desbloquear todo o potencial de seus dados e alcançar seus objetivos de negócios de maneira abrangente e eficaz.
+  ''';
+
+
+  static String consultorsOpiniumHeader = 'Descubra o que nossos consultores tem a dizer sobre a plataforma';
+}
