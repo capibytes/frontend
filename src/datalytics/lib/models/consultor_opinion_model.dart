78328423b6695024ab828432193820ccb6b558b8
mozart-rueckert <suporte4@grupoinovasse.com.br>
2023-10-29 23:35:18 -0300

    feat: model para card de opiniao do consultor

---
+++ src/datalytics/lib/models/consultor_opinion_model.dart
@@ -0,0 +1,11 @@
+class ConsultorOpinionModel {
+  String consultorName;
+  String cosultorAvatar;
+  String consultorComment;
+
+  ConsultorOpinionModel({
+    required this.consultorName,
+    required this.cosultorAvatar,
+    required this.consultorComment,
+  });
+}
