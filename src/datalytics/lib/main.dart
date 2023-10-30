ea19d94a804a7271df6e3e4883ad1482f6319eca
mozart-rueckert <suporte4@grupoinovasse.com.br>
2023-10-30 20:54:02 -0300

    feat: alterado titulo do projeto

--- src/datalytics/lib/main.dart
+++ src/datalytics/lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Datalytics',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: dtlGreyBlue),
