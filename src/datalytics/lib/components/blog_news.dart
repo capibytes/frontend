9f55ff638c3917a2495d0933ee44157fc94333e2
mozart-rueckert <suporte4@grupoinovasse.com.br>
2023-10-30 20:08:06 -0300

    feat: refatorado para responsividade

--- src/datalytics/lib/components/blog_news.dart
+++ src/datalytics/lib/components/blog_news.dart
@@ -4,57 +4,86 @@ import 'package:flutter/material.dart';
 
 class BlogNews extends StatelessWidget {
   final BlogNewsModel blogNews;
+  final double width;
   const BlogNews({
     required this.blogNews,
+    required this.width,
     super.key
   });
 
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Container(
-      width: ((size.width /4) * 3) - 80,
-      padding: const EdgeInsets.all(12),
-      decoration: BoxDecoration(
-        color: dtlGrey100,
-        borderRadius: BorderRadius.circular(8),
-      ),
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              blogNews.title,
-              textAlign: TextAlign.center,
-              overflow: TextOverflow.clip,
-              style: const TextStyle(
-                color: dtlBlack,
-                fontSize: 18,
-                fontWeight: FontWeight.w400,
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        double maxWidth = constraints.maxWidth;
+
+        return Container(
+          width: width,
+          padding: const EdgeInsets.all(12),
+          decoration: BoxDecoration(
+            color: dtlGrey100,
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Image.asset(
-                  blogNews.imageUrl,
-                  width: (size.width /4) - 80,
-                ),
-                SizedBox(
-                  width: ((size.width /4) * 2) - 80,
-                  child: Text(
-                    blogNews.content,
-                    textAlign: TextAlign.justify,
-                    overflow: TextOverflow.clip,
-                    maxLines: 16,
+                Text(
+                  blogNews.title,
+                  textAlign: TextAlign.center,
+                  overflow: TextOverflow.clip,
+                  style: const TextStyle(
+                    color: dtlBlack,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w400,
                   ),
                 ),
+                const SizedBox(height: 12,),
+                maxWidth < 800 
+                ? Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Image.asset(
+                      blogNews.imageUrl,
+                      width: 240,
+                    ),
+                    const SizedBox(height: 12,),
+                    SizedBox(
+                      width: (width * 0.8),
+                      child: Text(
+                        blogNews.content,
+                        textAlign: TextAlign.justify,
+                        overflow: TextOverflow.clip,
+                        maxLines: 16,
+                      ),
+                    ),
+                  ],
+                )
+                : Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Image.asset(
+                      blogNews.imageUrl,
+                      width: (size.width /4) - 80,
+                    ),
+                    SizedBox(
+                      width: ((size.width /4) * 2) - 80,
+                      child: Text(
+                        blogNews.content,
+                        textAlign: TextAlign.justify,
+                        overflow: TextOverflow.clip,
+                        maxLines: 16,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 12,),
               ],
             ),
-            const SizedBox(height: 12,),
-          ],
-        ),
-      ),
+          ),
+        );
+      }
     );
   }
 }
